f54557b5340dd23243b5cf8859280afe9e334183
Saktheeswarii <sakthidharmaraja@gmail.com>
2024-03-13 10:08:39 +0530

    printmul file is updated

--- PrintMultiple/printmul.js
+++ PrintMultiple/printmul.js
@@ -1,6 +1,6 @@
-var d=document.getElementById("res")
 function update()
 {
+    var d=document.getElementById("res")
     var list=document.createElement("h1")
     list.textContent="Hello"
     d.append(list)
