3e5c1aba657d8c2679ce1d0df7fb4737c0911b87
Saktheeswarii <sakthidharmaraja@gmail.com>
2024-03-13 12:34:52 +0530

    todo.js file is updated

--- TodoList/todo.js
+++ TodoList/todo.js
@@ -1,1 +1,14 @@
+function add()
+{
+    var ul=document.getElementById("ulist")
+    var input=document.getElementById("input")
+    var list=document.createElement("li")
+    list.innerHTML=input.value+"<button onclick='deletee(event)'>Delete</button>"
+    input.value=""
+    ul.append(list)
+}
+function deletee(event)
+{
+    event.target.parentElement.remove()
+}
 
