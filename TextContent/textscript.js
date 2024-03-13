b9ae861e48dbcfae19fba2d04fe067a5debef53e
Saktheeswarii <sakthidharmaraja@gmail.com>
2024-03-13 10:09:22 +0530

    textscript file is updated

--- TextContent/textscript.js
+++ TextContent/textscript.js
@@ -1,6 +1,6 @@
-var inp=document.getElementById("one")
-var res=document.getElementById("result")
 function call()
 {
+    var inp=document.getElementById("one")
+    var res=document.getElementById("result")
     res.textContent=inp.value
 }
