f950640ab2ae40a1575ea72c11e4820ed1b7bace
Saktheeswari <137200954+Saktheeswarii@users.noreply.github.com>
2024-03-14 01:17:28 +0530

    studentscript.js file is updated

--- StudentRegistrationForm/studentscript.js
+++ StudentRegistrationForm/studentscript.js
@@ -1,1 +1,38 @@
-
+function update()
+    {
+        var name=document.getElementById("name")
+        var age=document.getElementById("age")
+        var gender=getSelectedGender()
+        var course=getSelectedCourse() 
+        var email=document.getElementById("email")
+        var table=document.getElementById("res")
+        var list=document.createElement("tr")
+        list.innerHTML='<td>'+name.value+'</td>'+'<td>'+age.value+'</td>'+'<td>'+course+'</td>'+'<td>'+gender+'</td>'+'<td>'+email.value+'</td>'+'<td><button onclick="deletelist(this)">Delete</button></td>';
+        table.appendChild(list)
+    }
+    function deletelist(row) {
+            var i = row.parentNode.parentNode.rowIndex;
+            document.getElementById("res").deleteRow(i);
+        }
+    function getSelectedGender() 
+    {
+        if (document.getElementById('male').checked) 
+        {
+        return document.getElementById('male').value; 
+        } 
+        else if (document.getElementById('female').checked)
+        {
+        return document.getElementById('female').value;
+        } 
+        else {
+        return null; 
+        }
+    }
+    function getSelectedCourse() 
+    {
+        var selectElement = document.getElementById("course");
+        var selectedOption = selectElement.options[selectElement.selectedIndex];
+        var selectedValue = selectedOption.value;
+        console.log(selectedValue)
+        return selectedValue;
+    }
