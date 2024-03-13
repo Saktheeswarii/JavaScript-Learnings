57af2a11e61ed2b9da1c773bf21d655e2a192793
Saktheeswarii <sakthidharmaraja@gmail.com>
2024-03-13 09:33:10 +0530

    script file is updated

---
+++ Add/script.js
@@ -0,0 +1,15 @@
+function result()
+{
+
+    var n1=document.getElementById("num1")
+    var n2=document.getElementById("num2")
+    var result=document.getElementById("result")
+
+    var a=Number(n1.value)
+    var b=Number(n2.value)
+
+    var total= a+b
+    
+
+        para.textContent="Result="+total
+}
