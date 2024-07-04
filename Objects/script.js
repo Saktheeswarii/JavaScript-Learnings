2d218af3d1d316003df9fec7299e11bbdf48413e
Saktheeswari <137200954+Saktheeswarii@users.noreply.github.com>
2024-07-04 14:28:25 +0530

    commit

--- Objects/script.js
+++ Objects/script.js
@@ -7,3 +7,28 @@ console.log(typeof(car));
 car.type="Toyota";
 car.wheels="4wheels";
 console.log(car);
+
+// object methods
+
+// function greet()
+// {
+//     return "Hello, my name is " + person.name + " I am " + person.age + " years old.";
+// }
+
+// const person={
+//     name : "John",
+//     age : 18,
+//     greet,
+// };
+
+// console.log(person.greet());
+
+const person={
+    name : "John",
+    age : 18,
+    greet : function greet(){
+         return "Hello, my name is " + person.name + " I am " + person.age + " years old.";
+     }
+};
+
+console.log(person.greet());
