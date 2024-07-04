e1c1a7878e383ffc2428f11c720cad4e9596fc7d
Saktheeswari <137200954+Saktheeswarii@users.noreply.github.com>
2024-07-04 10:29:54 +0530

    commit

---
+++ Objects/script.js
@@ -0,0 +1,11 @@
+const person={
+    name : "xyz",
+    age:12,
+    location : ["xx","yy"],
+    isprogrammer : true,
+};
+console.log(person.name);
+console.log(person.age);
+console.log(person.location[0]);
+console.log(person.location[1]);
+console.log(person.isprogrammer); 
