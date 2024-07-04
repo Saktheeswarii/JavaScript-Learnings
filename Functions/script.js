50ce142be12bc000c14bb6b8bfc9c4ee5e0e9e6a
Saktheeswari <137200954+Saktheeswarii@users.noreply.github.com>
2024-07-04 13:27:58 +0530

    commit

--- Functions/script.js
+++ Functions/script.js
@@ -20,3 +20,35 @@ function myfunction(a,b)
     return a*b;
 }
 console.log(myfunction(3,4));
+
+// function expression
+
+const greetings = function (user)
+{
+    console.log("Hello "+ user);
+}
+
+greetings("kathy");
+
+// callback function
+
+function greett(name,cb)
+{
+    console.log("Hello "+name);
+    cb();
+}
+
+
+greett("John",function cb(){
+    console.log("I am a call back function");
+});
+
+function showcallfunc(fn){
+    let value=10;
+    fn(value);
+}
+function fn(value){
+    console.log("Value="+value);
+}
+
+showcallfunc(fn);
