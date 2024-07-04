21743bb35de874c84dd78b2f02921d486123d2c5
Saktheeswari <137200954+Saktheeswarii@users.noreply.github.com>
2024-07-04 11:20:01 +0530

    commit

---
+++ Functions/script.js
@@ -0,0 +1,22 @@
+function greet(){
+    console.log("Welcome!");
+}
+
+greet();
+
+function sayhello(name){
+    console.log("Hello "+ name);
+}
+
+sayhello("kathy");
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(5,3));
+
+function myfunction(a,b)
+{
+    return a*b;
+}
+console.log(myfunction(3,4));
